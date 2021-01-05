3257b97b9e176d58e1d91dcea3fce2a56b85c660
nageshmv06 <nageshmv18@gmail.com>
2021-01-05 22:47:26 +0530

    Updates

---
+++ precache-manifest.ecf44b55bf704176e850cd8b619d191b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4cb7817397c7a1debf545f6f038ddf",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "e6774bd20fcc85a4d3a5",
+    "url": "/slider/static/css/main.acdc85d5.chunk.css"
+  },
+  {
+    "revision": "fc03e981ffa576d9feb9",
+    "url": "/slider/static/js/2.1770a8ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider/static/js/2.1770a8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6774bd20fcc85a4d3a5",
+    "url": "/slider/static/js/main.6e95fe28.chunk.js"
+  },
+  {
+    "revision": "1cad62171374d2709340",
+    "url": "/slider/static/js/runtime-main.48ebb478.js"
+  }
+]);
